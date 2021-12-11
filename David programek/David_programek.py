052ccfe2b6fd2e6a17a0e6f6d6bc8c176f955461
LukysGaming <xdmoj1@seznam.cz>
2021-12-11 22:38:42 +0100

    Add project files.

---
+++ David programek/David_programek.py
@@ -0,0 +1,7 @@
+david_sees_his_anime = input("Dívá se David na jeho list watched anime?\nAno / Ne\n")
+if david_sees_his_anime == "ano":
+    while 1:
+        print("dobry anime")
+        continue
+else:
+    print("Napsal jsi něco jiného než ano nebo se David nedívá na jeho list anime.")
